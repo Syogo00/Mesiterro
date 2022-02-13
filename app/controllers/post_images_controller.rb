4369c6de9bbb891e39a7ce25ac95cee01a6315d1
EC2 Default User <ec2-user@ip-172-31-86-83.ec2.internal>
2022-02-13 14:38:18 +0000

    {finish}4-15

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -20,14 +20,14 @@ class PostImagesController < ApplicationController
 
   def destroy
     @post_image = PostImage.find(params[:id])
-    @post_image.derete
+    @post_image.destroy
     redirect_to post_images_path
   end
 
   private
 
   def post_image_params
-    params.require(:post_image).permit(:shop_name,:image, :caption)
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 
 end
